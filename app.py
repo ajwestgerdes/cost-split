2cc4688b15ae6e312a8080e9fbeebab08af44d95
Andrew Westgerdes <ajwestgerdes@gmail.com>
2023-07-21 22:23:41 -0500

    getting form info from webpage added rows button

--- app.py
+++ app.py
@@ -6,9 +6,15 @@ app = Flask(__name__, template_folder='.')
 def index():
     return render_template('index.html')
 
-# @app.route('/submit')
-# def submit():
-#     return
+@app.route('/split', methods=['POST'])
+def split():
+    form_data = request.form
+    print(form_data)
+
+    for key, value in form_data.items():
+        print(f"{key}: {value}")
+
+    return 'Form submitted successfully!'
 
 if __name__ == '__main__':
     app.run(debug=True)
